3c24fb46836074e23e03354523eddf5270e0b8d8
TGregorio11 <thiago.gregorio11@gmail.com>
2024-04-10 11:17:28 -0300

    ajustes

--- cypress/e2e/zlogin.cy.js
+++ cypress/e2e/zlogin.cy.js
@@ -1,4 +1,4 @@
-<reference types="cypress" />
+///<reference types="cypress" />
 let dadosLogin
 
 context('Funcionalidade Login', () => {
@@ -33,6 +33,6 @@ context('Funcionalidade Login', () => {
         cy.get('#password').type(dadosLogin.senha, { log: false })
         cy.get('.woocommerce-form > .button').click()
         cy.get('.page-title').should('contain', 'Minha conta')
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
+        //cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
     })
 })
